5a20d06efbdd4a2122ffceb0d11803f4054b42df
dingjiaao <1157607108@qq.com>
2025-12-30 14:15:10 +0800

    用户模块--分页，返回精度调整

---
+++ src/main/java/com/ding/dingpicturebackend/config/MyBatisPlusConfig.java
@@ -0,0 +1,22 @@
+package com.ding.dingpicturebackend.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.ding.dingpicturebackend.mapper")
+public class MyBatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor myBatisPlusConfigInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
